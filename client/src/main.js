3b5893597ed2b14a39dba5388ea60599c2354773
iovitz <iovitz1999@gmail.com>
2024-03-28 12:05:42 +0800

    chore: 删除冗余代码

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,9 @@
 import App from "./App.vue";
-import uvUI from "@climblee/uv-ui";
 import { createSSRApp } from "vue";
 import { pinia } from "./store";
 
 export function createApp() {
 	const app = createSSRApp(App);
-	app.use(uvUI);
 	app.use(pinia);
 	return {
 		app,
