9abf69fab9f84ed526a66c01bed9d4ce6c6131ee
iovitz <iovitz1999@gmail.com>
2024-04-02 15:44:51 +0800

    chore: 日志页面开发

--- client/src/pages/calendar/calendar.vue
+++ client/src/pages/calendar/calendar.vue
@@ -1,14 +1,6 @@
 <template>
   <h1>Calendar</h1>
-  <TabBar path="/pages/calendar/calendar" />
-  <canvas
-    canvas-id="canvas"
-    class="canvas"
-    :start="startStatus"
-    :change:start="animate.start"
-    :data-width="canvasWidth"
-    :data-height="canvasWidth"
-  ></canvas>
+  <TabBar path="/pages/message/message" />
 </template>
 
 <script setup>
@@ -16,7 +8,6 @@ import TabBar from "@/components/tabbar/tabbar.vue";
 import { useHideTabBar } from "@/hooks/hide-tabbar";
 
 useHideTabBar();
-console.log("###");
 </script>
 
 <style lang="scss" scoped></style>
