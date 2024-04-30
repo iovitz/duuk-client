abef0eaab654d1617593f9ff674c557065180a19
iovitz <iovitz1999@gmail.com>
2024-04-30 11:07:35 +0800

    wip: 搜索页开发

--- src/pages/search/search.vue
+++ src/pages/search/search.vue
@@ -1,13 +1,41 @@
 <template>
   <view class="page-container tabbar">
-    <uv-navbar title="搜索" :placeholder="true" :autoBack="true" left-icon="">
-      <template #right>
-        <text class="dicon dicon-asterism"></text>
-      </template>
-    </uv-navbar>
+    <uv-status-bar />
+    <view class="p-2 bg-white">
+      <uv-search :showAction="true" actionText="搜索"></uv-search>
+      <uv-tabs
+        :list="tabList"
+        @click="handleTabClick"
+        :scrollable="false"
+      ></uv-tabs>
+    </view>
   </view>
 </template>
 
-<script setup></script>
+<script setup>
+import { logger } from "@/utils/logger";
+import { ref } from "@vue/runtime-core";
+
+const tabList = ref([
+  {
+    name: "综合",
+  },
+  {
+    name: "消息",
+  },
+  {
+    name: "文章",
+  },
+  {
+    name: "用户",
+  },
+  {
+    name: "群组",
+  },
+]);
+const handleTabClick = (e) => {
+  logger.debug("切换tab", e);
+};
+</script>
 
 <style lang="scss" scoped></style>
